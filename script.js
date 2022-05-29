efe13c304dee55c9801b43d12f7764f453111211
Tanishka Talwar <tanishkatalwar18@gmail.com>
2022-05-29 14:58:31 +0530

    display whose turn is it and change turn o each click

--- script.js
+++ script.js
@@ -5,4 +5,6 @@ container.addEventListener('click',function(e){
     if(div.className === 'x' || div.className === 'o') return
     div.className = turn;
     turn = turn === 'x' ? 'o' : 'x';
+    div.parentElement.className = turn === 'x' ? 'container x-turn' : 'container o-turn';
+    console.log(div.getAttribute('data-number'));
 });
