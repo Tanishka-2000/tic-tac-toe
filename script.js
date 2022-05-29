4e8aeb805cd1a658787a694ca0c053e0349491ff
Tanishka Talwar <tanishkatalwar18@gmail.com>
2022-05-29 14:30:47 +0530

    add event listener to container to show x or o on click

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-
+let turn = 'x';
+let container = document.querySelector('.container');
+container.addEventListener('click',function(e){
+    let div = e.target;
+    if(div.className === 'x' || div.className === 'o') return
+    div.className = turn;
+    turn = turn === 'x' ? 'o' : 'x';
+});
