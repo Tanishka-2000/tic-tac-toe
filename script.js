851fd2bc38ebf4769398edf6cc5227928fdbebbb
Tanishka Talwar <tanishkatalwar18@gmail.com>
2022-05-30 00:19:39 +0530

    add function to check win and display winner on messageDiv

--- script.js
+++ script.js
@@ -1,44 +1,58 @@
 
-const winningCombinations = [[1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9], [3,5,7]];
-// const playerX = [];
-// const playerO = [];
-// let turn = 'x';
-
-
 const game = (function(){
+    const winningCombinations = [[1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9], [3,5,7]];
     const playerX = [];
     const playerO = [];
     let div;
     let turn = 'x';
-
-    const divInvalid = () => div.className === '' ? false : true;
-    const addDivNumber = num => turn === 'x' ? playerX.push(num) : playerO.push(num);
+    let chance = 0;
+    let winner;
+    const addDivNumber = () => {
+        let num = Number(div.getAttribute('data-number'));
+        turn === 'x' ? playerX.push(num) : playerO.push(num);
+    }
     const changeTurn = () => turn = turn === 'x' ? 'o' : 'x';
     const addMarkerClass = () => div.className = turn;
     const changeTurnMessage = () => div.parentElement.className = turn === 'x' ? 'container x-turn' : 'container o-turn';
+    const gameReset = () => {
+        playerO.length = 0;
+        playerX.length = 0;
+        turn = 'x';
+        div = null;
+    }
+    const checkWin = () =>{
+        if(chance < 5) return;
+        if(turn === 'x'){
+            if(winningCombinations.some(arr => arr.every(num => playerX.includes(num)))) winner = 'x';
+        }else{
+            if(winningCombinations.some(arr => arr.every(num => playerO.includes(num)))) winner = 'o';
+        }
+    }
 
+    const gameOver = () => {
+        console.log(`${winner} wins!`);
+        messageDiv.style.display = 'block';
+        messageDiv.firstElementChild.textContent = `${winner.toUpperCase()} Win !`;
+    }
     const markDiv = (e) => {
         div = e.target;
-        if(divInvalid()) return;
-        addDivNumber(div.getAttribute('data-number'));
+        if(div.className !== '') return;
+        addDivNumber();
         addMarkerClass();
+        chance++;
+        checkWin();
+        if(winner) gameOver();
+
         changeTurn();
         changeTurnMessage();
     }
-    return {markDiv};
+    return {markDiv, gameReset};
 })();
 
 let container = document.querySelector('.container');
 container.addEventListener('click', game.markDiv);
 
-// function markDiv(e){
-//     let div = e.target;
-//     if(div.className === 'x' || div.className === 'o') return
-//     div.className = turn;
-//     turn = turn === 'x' ? 'o' : 'x';
-//     div.parentElement.className = turn === 'x' ? 'container x-turn' : 'container o-turn';
-//     console.log(div.getAttribute('data-number'));
-// }
+const messageDiv = document.querySelector('.message');
 
 
 // 1. Game starts
